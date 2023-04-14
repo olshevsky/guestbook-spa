d156d568991338af2a88bc2f1b5cde12e843b3fc
dev <artur.olshevsky@gmail.com>
2023-04-14 22:56:34 +0300

    init

---
+++ resources/js/Pages/Guestbook/Index.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="p-6">
+        <h1 class="text-2xl font-bold mb-6">Messages</h1>
+        <Link href="/guestbook/create" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
+            <span>Create message</span>
+        </Link>
+        <table class="table-auto w-full">
+            <thead>
+            <tr>
+                <th class="px-4 py-2 text-left">Name</th>
+                <th class="px-4 py-2 text-left">Message</th>
+                <th class="px-4 py-2 text-left">Created At</th>
+                <th class="px-4 py-2 text-left">Actions</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr v-for="message in messages" :key="message.id">
+                <td class="border px-4 py-2">
+                    <span>{{ message.name }} &nbsp;</span>
+                    <a :href="'mailto:' + message.email">{{ message.email }}</a>
+                </td>
+                <td class="border px-4 py-2">
+                    {{ message.message }}
+                    <a href="" target="_blank">
+                        <img :src="message.image" class="max-w-md">
+                    </a>
+                    <p class="italic">Edited: 01.01.2023 08:00:00</p>
+                </td>
+                <td class="border px-4 py-2">{{ message.created_at }}</td>
+                <td class="border px-4 py-2">
+                    <button @click="editMessage(message)" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline mr-2">Edit</button>
+                    <button @click="deleteMessage(message)" class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">Delete</button>
+                </td>
+            </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script setup>
+
+import { Link } from '@inertiajs/vue3'
+
+const props = defineProps(['messages']);
+
+const editMessage = (message) => {
+    alert('edit')
+}
+
+const deleteMessage = (message) => {
+    alert('delete')
+}
+</script>
---
+++ routes/web.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Foundation\Application;
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+use App\Http\Controllers\GuestbookController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return Inertia::render('Welcome', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
+    ]);
+});
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return Inertia::render('Dashboard');
+    })->name('dashboard');
+});
+
+Route::get('guestbook', [GuestbookController::class, 'index'])->name('guestbook');
+Route::get('guestbook/create', [GuestbookController::class, 'create'])->name('guestbook.create');
+Route::post('guestbook', [GuestbookController::class, 'store'])->name('guestbook.store');
+Route::get('guestbook/{message}/edit', [GuestbookController::class, 'edit'])->name('guestbook.edit');
+Route::put('guestbook/{message}', [GuestbookController::class, 'update'])->name('guestbook.update');
+Route::delete('guestbook/{message}', [GuestbookController::class, 'destroy'])->name('guestbook.destroy');
+Route::put('guestbook/{message}/restore', [GuestbookController::class, 'restore'])->name('guestbook.restore');
