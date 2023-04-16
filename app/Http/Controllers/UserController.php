7f427b7bee62a1300959d413332ea721036e695e
dev <artur.olshevsky@gmail.com>
2023-04-16 13:42:45 +0300

    rm password

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -46,8 +46,7 @@ class UserController extends Controller
             'user' => [
                 'id' => $user->id,
                 'name' => $user->name,
-                'email' => $user->email,
-                'password' => $user->password
+                'email' => $user->email
             ],
         ]);
     }
