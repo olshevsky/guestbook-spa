989f4c00bc39038041c6d7c8c193674e8640b315
dev <artur.olshevsky@gmail.com>
2023-04-16 13:04:12 +0300

    users admin by default

--- database/migrations/2023_04_16_102008_user_admin.php
+++ database/migrations/2023_04_16_102008_user_admin.php
@@ -12,7 +12,8 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->boolean("is_admin");
+            $table->boolean("is_admin")
+                ->default(true);
         });
     }
 
